8a0e8196caac13e37de4ae88cc45f266864e7c10
Burak Koç <74610612+burakkoc5@users.noreply.github.com>
2025-01-14 03:21:21 +0300

    Update DemoSection.tsx

--- src/components/demo/DemoSection.tsx
+++ src/components/demo/DemoSection.tsx
@@ -146,7 +146,7 @@ const DemoSection = () => {
               playsInline
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
             >
-              <source src="/videos/demo.mp4" type="video/mp4" />
+              <source src="/images/demo.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </VideoContainer>
